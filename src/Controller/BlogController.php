b88c4800e14fa04c48add666a9635b6549772529
Katarzyna Głodny <support@keos.pl>
2020-09-12 18:30:31 +0200

    Added layout and other stuff

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/", name="blog")
+     */
+    public function index()
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+}
