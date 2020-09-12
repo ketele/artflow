856a70c681bee0a20855911b336e6963c12297d1
Katarzyna Głodny <support@keos.pl>
2020-09-12 18:44:31 +0200

    Added HomePage controller

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -5,15 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class BlogController extends AbstractController
+class HomePageController extends AbstractController
 {
     /**
-     * @Route("/blog", name="blog")
+     * @Route("/", name="home")
      */
     public function index()
     {
-        return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+        return $this->render('home_page/index.html.twig', [
+            'controller_name' => 'HomePageController',
         ]);
     }
 }
