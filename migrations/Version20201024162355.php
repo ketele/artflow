1f34f56697b358bc077efc13c0708115941ed716
Katarzyna Głodny <support@keos.pl>
2020-10-24 21:09:52 +0200

    Fixed migration bug

--- migrations/Version20201024162355.php
+++ migrations/Version20201024162355.php
@@ -21,13 +21,11 @@ final class Version20201024162355 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE doodle ADD popularity INT NOT NULL DEFAULT 0, ADD views INT NOT NULL DEFAULT 0');
-        $this->addSql('ALTER TABLE doodle_status CHANGE is_active is_active TINYINT(1) DEFAULT \'1\' NOT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE doodle DROP popularity, DROP views');
-        $this->addSql('ALTER TABLE doodle_status CHANGE is_active is_active TINYINT(1) DEFAULT \'1\' NOT NULL');
     }
 }
